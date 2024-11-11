1d2fa6e38029733508c160816599431daf44f132
Ishaanmuj <143868177+Ishaanmuj@users.noreply.github.com>
2024-11-11 10:03:31 +0530

    Add files via upload

---
+++ LAB6/2.py
@@ -0,0 +1,7 @@
+class Student:
+    def __init__(self, name, roll_no):
+        self.name = name
+        self.roll_no = roll_no
+
+student = Student("John", 2)
+print(student.name, student.roll_no)
